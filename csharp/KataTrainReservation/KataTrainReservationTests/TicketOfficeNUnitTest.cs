ae68cd79ca65ecfd0cd73eb5b4167cfeafad3914
kodjocod <kodjocod@gmail.com>
2020-03-02 10:39:47 +0100

    Create the sln C#, the projects and update the gitignore for c# files

---
+++ csharp/KataTrainReservation/KataTrainReservationTests/TicketOfficeNUnitTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace KataTrainReservation
+{
+    [TestFixture]
+    public class TicketOfficeNUnitTest {
+    
+        [Test]
+        public void reserveSeats() {
+            // TODO: write this code
+
+
+        }
+    }
+}
